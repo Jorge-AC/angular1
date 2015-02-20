79d5488466a24e106f192ba71d80ec48feab35d1
smac008 <smac008@iMac-de-smac008.local>
2015-02-20 18:29:45 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 
 
-var app = angular.module('timezoneFinderApp', []);
+var app = angular.module('timezoneFinderApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) { 
+
+	$locationProvider.html5Mode(true);
+	$routeProvider.
+    when('/welcome', {
+      templateUrl: 'welcome.html',
+      controller: 'MainController'
+    }).
+    when('/list', {
+      templateUrl: 'list.html',
+      controller: 'MainController'
+    }).
+    otherwise({
+      redirectTo: '/welcome'
+	}); 
+}]);
 
 app.controller('MainController', [ '$scope', function($scope){
 		// Defines our cities.
@@ -11,7 +28,7 @@ app.controller('MainController', [ '$scope', function($scope){
 	    { name: 'Los Angeles', timezone: 'PST' },
 	    { name: 'New York', timezone: 'EST' }
 	];
-	
+
 	// The default city.
 	$scope.selectedCity = $scope.cities[0];
 }])
